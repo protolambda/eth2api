c00ba6a795be6321a7ee0bd830f55cf462dc5f6f
protolambda <proto@protolambda.com>
2020-12-21 00:32:16 +0100

    fix debug beacon head and state endpoints

--- debugapi/state.go
+++ debugapi/state.go
@@ -7,6 +7,6 @@ import (
 )
 
 // Returns full BeaconState object for given stateId.
-func State(ctx context.Context, cli eth2api.Client, stateId eth2api.StateId, dest *beacon.BeaconState) (exists bool, err error) {
-	return eth2api.SimpleRequest(ctx, cli, eth2api.FmtGET("eth/v1/debug/state/%s", stateId.StateId()), eth2api.Wrap(dest))
+func BeaconState(ctx context.Context, cli eth2api.Client, stateId eth2api.StateId, dest *beacon.BeaconState) (exists bool, err error) {
+	return eth2api.SimpleRequest(ctx, cli, eth2api.FmtGET("eth/v1/debug/beacon/states/%s", stateId.StateId()), eth2api.Wrap(dest))
 }
